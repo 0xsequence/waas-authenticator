5c6a7c5a78761d74dea2d8989845052d770f4575
Patryk Kalinowski <patryk@kalinowski.dev>
2024-02-01 18:03:25 +0100

    Add Account to proto and refactor Identity

--- proto/identity.go
+++ proto/identity.go
@@ -1,17 +1,37 @@
 package proto
 
 import (
-	"strconv"
+	"fmt"
+	"strings"
 )
 
 func (id Identity) String() string {
-	return strconv.Itoa(int(id.ProjectID)) + "#" + id.Issuer + "#" + id.Subject
+	switch id.Type {
+	case IdentityType_OIDC:
+		return id.Type.String() + ":" + id.Issuer + "#" + id.Subject
+	default:
+		return ""
+	}
 }
 
-func (s SessionData) Identity() Identity {
-	return Identity{
-		ProjectID: s.ProjectID,
-		Issuer:    s.Issuer,
-		Subject:   s.Subject,
+func (id *Identity) FromString(s string) error {
+	parts := strings.SplitN(s, ":", 2)
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid identity format: %s", s)
 	}
+
+	switch IdentityType(IdentityType_value[parts[0]]) {
+	case IdentityType_OIDC:
+		oidcParts := strings.SplitN(parts[1], "#", 2)
+		if len(oidcParts) != 2 {
+			return fmt.Errorf("invalid OIDC identity format: %s", parts[1])
+		}
+		id.Type = IdentityType_OIDC
+		id.Issuer = oidcParts[0]
+		id.Subject = oidcParts[1]
+
+	default:
+		return fmt.Errorf("invalid identity type: %s", parts[0])
+	}
+	return nil
 }
