c8d05dba9e105a1d69136e53b73eed973aec0a53
Patryk Kalinowski <patryk@kalinowski.dev>
2024-02-05 20:22:05 +0100

    Move old RPCs to v0_wrappers and add new v1 router

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -44,6 +44,8 @@ type RPC struct {
 	Accounts   *data.AccountTable
 	Wallets    proto_wallet.Wallet
 
+	V0 V0
+
 	startTime time.Time
 	running   int32
 }
@@ -108,6 +110,7 @@ func New(cfg *config.Config, client HTTPClient) (*RPC, error) {
 		Wallets:   proto_wallet.NewWalletClient(cfg.Endpoints.WaasAPIServer, client),
 		startTime: time.Now(),
 	}
+	s.V0 = V0{RPC: s} // TODO: remove
 	return s, nil
 }
 
@@ -203,7 +206,16 @@ func (s *RPC) Handler() http.Handler {
 		// Find and decrypt tenant data
 		r.Use(tenant.Middleware(s.Tenants, s.Config.KMS.TenantKeys))
 	})
-	userRouter.Handle("/rpc/WaasAuthenticator/*", proto.NewWaasAuthenticatorServer(s))
+	userRouter.Handle("/rpc/WaasAuthenticator/*", proto.NewWaasAuthenticatorServer(&s.V0))
+
+	userRouterV1 := r.Group(func(r chi.Router) {
+		// Generate attestation document
+		r.Use(attestation.Middleware(s.Enclave))
+
+		// Find and decrypt tenant data
+		r.Use(tenant.Middleware(s.Tenants, s.Config.KMS.TenantKeys))
+	})
+	userRouterV1.Handle("/rpc/WaasAuthenticatorV1/*", proto.NewWaasAuthenticatorV1Server(s))
 
 	adminRouter := r.Group(func(r chi.Router) {
 		// Validate admin JWTs
