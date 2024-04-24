ca3b11e0283e92e1ccb6cf2069f8bb98fe641b78
Patryk Kalinowski <patryk@kalinowski.dev>
2024-04-24 13:10:30 +0200

    rpc: identity verification with cached JWKS keys

---
+++ rpc/identity/keyset.go
@@ -0,0 +1,97 @@
+package identity
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/goware/cachestore"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
+
+// operationKeySet is a jwk.Set valid only for a single request.
+// It is not thread safe and should not be stored. Instead, it must be discarded after every operation.
+type operationKeySet struct {
+	ctx       context.Context
+	iss       string
+	store     cachestore.Store[jwk.Key]
+	getKeySet func(ctx context.Context, iss string) (jwk.Set, error)
+
+	cachedSet jwk.Set
+}
+
+func (ks *operationKeySet) LookupKeyID(s string) (jwk.Key, bool) {
+	ttl := 1 * time.Hour
+	getter := func(ctx context.Context, _ string) (jwk.Key, error) {
+		key, ok := ks.getCachedSet(ctx).LookupKeyID(s)
+		if !ok {
+			return nil, fmt.Errorf("key not found")
+		}
+		return key, nil
+	}
+
+	key, err := ks.store.GetOrSetWithLockEx(ks.ctx, ks.iss+"#"+s, getter, ttl)
+	if err != nil {
+		return nil, false
+	}
+	return key, true
+}
+
+func (ks *operationKeySet) AddKey(key jwk.Key) error {
+	return fmt.Errorf("operationKeySet is immutable")
+}
+
+func (ks *operationKeySet) Clear() error {
+	return fmt.Errorf("operationKeySet is immutable")
+}
+
+func (ks *operationKeySet) Key(i int) (jwk.Key, bool) {
+	return ks.getCachedSet(ks.ctx).Key(i)
+}
+
+func (ks *operationKeySet) Get(s string) (interface{}, bool) {
+	return ks.getCachedSet(ks.ctx).Get(s)
+}
+
+func (ks *operationKeySet) Set(s string, i interface{}) error {
+	return fmt.Errorf("operationKeySet is immutable")
+}
+
+func (ks *operationKeySet) Remove(s string) error {
+	return fmt.Errorf("operationKeySet is immutable")
+}
+
+func (ks *operationKeySet) Index(key jwk.Key) int {
+	return ks.getCachedSet(ks.ctx).Index(key)
+}
+
+func (ks *operationKeySet) Len() int {
+	return ks.getCachedSet(ks.ctx).Len()
+}
+
+func (ks *operationKeySet) RemoveKey(key jwk.Key) error {
+	return fmt.Errorf("operationKeySet is immutable")
+}
+
+func (ks *operationKeySet) Keys(ctx context.Context) jwk.KeyIterator {
+	return ks.getCachedSet(ks.ctx).Keys(ctx)
+}
+
+func (ks *operationKeySet) Iterate(ctx context.Context) jwk.HeaderIterator {
+	return ks.getCachedSet(ks.ctx).Iterate(ctx)
+}
+
+func (ks *operationKeySet) Clone() (jwk.Set, error) {
+	return ks.getCachedSet(ks.ctx).Clone()
+}
+
+func (ks *operationKeySet) getCachedSet(ctx context.Context) jwk.Set {
+	set, err := ks.getKeySet(ctx, ks.iss)
+	if err != nil {
+		return jwk.NewSet()
+	}
+	ks.cachedSet = set
+	return set
+}
+
+var _ jwk.Set = (*operationKeySet)(nil)
---
+++ rpc/identity/oidc.go
@@ -0,0 +1,103 @@
+package identity
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/0xsequence/waas-authenticator/proto"
+	"github.com/0xsequence/waas-authenticator/rpc/tenant"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+)
+
+func (v *Verifier) GetKeySet(ctx context.Context, issuer string) (jwk.Set, error) {
+	jwksURL, err := fetchJWKSURL(ctx, v.client, issuer)
+	if err != nil {
+		return nil, fmt.Errorf("fetch issuer keys: %w", err)
+	}
+
+	keySet, err := jwk.Fetch(ctx, jwksURL, jwk.WithHTTPClient(v.client))
+	if err != nil {
+		return nil, fmt.Errorf("fetch issuer keys: %w", err)
+	}
+	return keySet, nil
+}
+
+func getOIDCProvider(ctx context.Context, issuer string) *proto.OpenIdProvider {
+	tntData := tenant.FromContext(ctx)
+	for _, idp := range tntData.OIDCProviders {
+		if idp.Issuer == issuer {
+			return idp
+		}
+	}
+	return nil
+}
+
+func fetchJWKSURL(ctx context.Context, client HTTPClient, iss string) (string, error) {
+	// Construct the URL to the issuer's .well-known/openid-configuration endpoint
+	issuerConfigURL := normalizeIssuer(iss) + "/.well-known/openid-configuration"
+
+	req, err := http.NewRequest(http.MethodGet, issuerConfigURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("failed to fetch openid configuration")
+	}
+
+	var config map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&config); err != nil {
+		return "", err
+	}
+
+	jwksURI, ok := config["jwks_uri"].(string)
+	if !ok {
+		return "", errors.New("jwks_uri not found in openid configuration")
+	}
+
+	return jwksURI, nil
+}
+
+func normalizeIssuer(iss string) string {
+	if !strings.HasPrefix(iss, "https://") && !strings.HasPrefix(iss, "http://") {
+		return "https://" + iss
+	}
+	return iss
+}
+
+func getEmailFromToken(tok jwt.Token) string {
+	// TODO: we might want to relax this a bit and not depend so much on the `email_verified` claim
+	emailVerifiedClaim, ok := tok.Get("email_verified")
+	if !ok {
+		return ""
+	}
+
+	verified := false
+	switch v := emailVerifiedClaim.(type) {
+	case bool:
+		verified = v
+	case string:
+		verified = strings.TrimSpace(strings.ToLower(v)) == "true"
+	}
+	if !verified {
+		return ""
+	}
+
+	emailClaim, ok := tok.Get("email")
+	if !ok {
+		return ""
+	}
+	email, _ := emailClaim.(string)
+	return email
+}
