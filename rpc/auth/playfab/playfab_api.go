6bfa5595f56b0d582c47ffee2d7165a737f84fa3
Patryk Kalinowski <patryk@kalinowski.dev>
2024-06-13 23:16:54 +0300

    rpc/auth: implement Playfab AuthProvider

---
+++ rpc/auth/playfab/playfab_api.go
@@ -0,0 +1,50 @@
+package playfab
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type response struct {
+	Code int `json:"code"`
+	Data struct {
+		AccountInfo accountInfo `json:"AccountInfo"`
+	} `json:"data"`
+}
+
+type accountInfo struct {
+	PlayFabID   string `json:"PlayFabId"`
+	PrivateInfo struct {
+		Email string `json:"Email"`
+	} `json:"PrivateInfo"`
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func getAccountInfo(ctx context.Context, titleID string, sessionTicket string, client HTTPClient) (*accountInfo, error) {
+	url := fmt.Sprintf("https://%s.playfabapi.com/Client/GetAccountInfo", titleID)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Authorization", sessionTicket)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %w", err)
+	}
+	defer res.Body.Close()
+
+	var resp response
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &resp.Data.AccountInfo, nil
+}
